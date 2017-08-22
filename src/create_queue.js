912c700149ce24fe05197d0dc386967706dfe680
Dan Pelensky <dan@pelensky.com>
2017-08-22 17:24:20 +0100

    can subscribe to queue and change permissions

--- src/create_queue.js
+++ src/create_queue.js
@@ -4,7 +4,16 @@ var sns = new AWS.SNS();
 var sqs = new AWS.SQS();
 
 const id = createUuid();
-let url
+const topicArn = 'arn:aws:sns:eu-west-1:549374948510:tic-tac-toe';
+
+function getQueueUrl(id) {
+  return "https://sqs.eu-west-1.amazonaws.com/549374948510/".concat(id);
+}
+
+function getEndpoint(id) {
+  return 'arn:aws:sqs:eu-west-1:549374948510:demo'.concat(id)
+}
+
 
 function createQueue() {
   sqs.createQueue({
@@ -15,9 +24,76 @@ function createQueue() {
       console.log(util.inspect(err));
       return;
     }
+    return result.QueueUrl;
+  });
+}
 
-    url = result.QueueUrl;
-    console.log (url);
+function getQueueInfo(id) {
+
+  sqs.getQueueAttributes({
+    QueueUrl: "https://sqs.eu-west-1.amazonaws.com/549374948510/".concat(id),
+    AttributeNames: ["QueueArn"]
+  }, function (err, result) {
+
+    if (err !== null) {
+      console.log(util.inspect(err));
+      return;
+    }
+    console.log(util.inspect(result));
+  });
+}
+
+function createSubscription(id) {
+  sns.subscribe({
+    'TopicArn': topicArn,
+    'Protocol': 'sqs',
+    'Endpoint': getEndpoint(id),
+  }, function (err, result) {
+
+    if (err !== null) {
+      console.log(util.inspect(err));
+      return;
+    }
+
+    console.log(util.inspect(result));
+
+  });
+}
+
+function addPermissions(id) {
+  var attributes = {
+    "Version": "2008-10-17",
+    "Id": getEndpoint(id) + "/SQSDefaultPolicy",
+    "Statement": [{
+      "Sid": "Sid" + new Date().getTime(),
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "*"
+      },
+      "Action": "SQS:SendMessage",
+      "Resource": getEndpoint(id),
+      "Condition": {
+        "ArnEquals": {
+          "aws:SourceArn": topicArn
+        }
+      }
+    }
+    ]
+  };
+
+  sqs.setQueueAttributes({
+    QueueUrl: getQueueUrl(id),
+    Attributes: {
+      'Policy': JSON.stringify(attributes)
+    }
+  }, function (err, result) {
+
+    if (err !== null) {
+      console.log(util.inspect(err));
+      return;
+    }
+
+    console.log(util.inspect(result));
   });
 }
 
@@ -28,4 +104,4 @@ function createUuid() {
   });
 }
 
-createQueue()
+addPermissions("demo");
